971ea4b43da3ecdbe6e667f7ca95ab33410d5ed5
Ashakibp <aaronshakib@gmail.com>
2019-04-05 01:17:54 -0400

    pushing ignore

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/signUp', signUpRouter);
+app.use('/signup', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/signup.js
+++ routes/signup.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/signup', function (req, res, next) {
-    res.render('signup', { title: 'Sign Up' });
+    res.render('test');
 });
 
 module.exports = router;
